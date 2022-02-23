87237a2cd2695e96f83c177f17da7c8a20a3eea8
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-23 12:24:57 -0400

    Update Productos.cs

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Productos_Detalle.Entidades
 {
@@ -11,14 +12,33 @@ namespace Productos_Detalle.Entidades
         
         [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string Descripcion { get; set; }
-        
-        public int Existencia { get; set; }
 
-        [Required(ErrorMessage = "El costo es obligatorio")]
-        [Range(1,int.MaxValue,ErrorMessage = "El costo debe estar entre los datos permitidos")]
+        [Required(ErrorMessage = "El Costo es obligatorio")]
+        [Range(1,float.MaxValue,ErrorMessage = "El costo debe estar entre los datos permitidos")]
         public float Costo { get; set; }
 
-        public float ValorInventario { get; set; }
+        [Required(ErrorMessage = "La Ganancia es obligatorio")]
+        [Range(1,float.MaxValue,ErrorMessage = "La Ganancia debe estar entre los datos permitidos")]
+        public float Ganancia { get; set; }
+
+        [Required(ErrorMessage = "El Precio es obligatorio")]
+        [Range(1,float.MaxValue,ErrorMessage = "El Precio debe estar entre los datos permitidos")]
+        public float Precio { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public List<ProductosDetalle> Detalle { get; set; } = new List<ProductosDetalle> ();
+    }
+
+    public class ProductosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+
+        public string Presentacion { get; set; }
+
+        public float Precio { get; set; }
 
     }
+
 }
