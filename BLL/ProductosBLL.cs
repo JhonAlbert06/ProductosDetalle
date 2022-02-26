3932ece36a17b9b14bb8d46e3d8cae996c32c67e
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-26 12:04:35 -0400

    Update ProductosBLL.cs

--- BLL/ProductosBLL.cs
+++ BLL/ProductosBLL.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
 using Productos_Detalle.DAL;
 using Productos_Detalle.Entidades;
 
