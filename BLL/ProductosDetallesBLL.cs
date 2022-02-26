a6af26a9dd19b188f0856ee73ef3261b2c7786b1
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-26 12:04:38 -0400

    Create ProductosDetallesBLL.cs

---
+++ BLL/ProductosDetallesBLL.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using Productos_Detalle.DAL;
+using Productos_Detalle.Entidades;
+
+namespace Productos_Detalle.BLL
+{
+    public class ProductosDetalleBLL
+    {
+        
+    }
+}
