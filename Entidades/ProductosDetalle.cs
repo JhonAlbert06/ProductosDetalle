861e5505416fa4a900488a7752240bac9a758d2f
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-27 18:37:02 -0400

    Update ProductosDetalle.cs

--- Entidades/ProductosDetalle.cs
+++ Entidades/ProductosDetalle.cs
@@ -8,10 +8,18 @@ namespace Productos_Detalle.Entidades
         [Key]
         public int Id { get; set; }
         public int ProductoId { get; set; }
-        public string? Descripcion { get; set; }
+        public string Descripcion { get; set; }
         public double Cantidad { get; set; }
         public double Precio { get; set; }
 
+        public ProductosDetalle()
+        {
+            this.Id = 0;
+            this.ProductoId = 0;
+            this.Descripcion = null;
+            this.Cantidad = 0;
+            this.Precio = 0;
+        }
         public ProductosDetalle(int productoId, string descripcion, double cantidad, double precio)
         {
             this.Id = 0;
