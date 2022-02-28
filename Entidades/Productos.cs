5a3eec2e908636965e0f4baac4f0f8fc2df62a12
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-28 09:48:46 -0400

    Update Productos.cs

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -10,16 +10,23 @@ namespace Productos_Detalle.Entidades
         
         [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string Descripcion { get; set; }
-        
+        /* 
+        [Required(ErrorMessage = "La existencia no puede estar vacio...")]
+        [Range(1, double.MaxValue, ErrorMessage = "La existencia debe estar en el rango de {1} y {2}.")]
         public double Existencia { get; set; }
         
+        [Required(ErrorMessage = "El campo Costo no puede estar vacio...")]
+        [Range(1, double.MaxValue, ErrorMessage = "El costo debe estar en el rango de {1} y {2}.")]
         public double Costo { get; set; }
 
+        
         public double ValorInventario { get; set; }
 
         public double Ganancia { get; set; }
-
-        public double Precio { get; set; }
+        
+        [Required(ErrorMessage = "El campo Precio no puede estar vacio...")]
+        [Range(1, double.MaxValue, ErrorMessage = "El Precio debe estar en el rango de {1} y {2}.")]
+        public double Precio { get; set; } */
 
         [ForeignKey("ProductoId")]
         public List<ProductosDetalle> Detalle { get; set; } = new List<ProductosDetalle> ();
