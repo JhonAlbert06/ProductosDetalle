bf940852a1ef9c19d9e72062965e21675ff99fab
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-28 09:48:26 -0400

    Migracion dos

---
+++ Migrations/20220228030026_segunda.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Productos_Detalle.Migrations
+{
+    public partial class segunda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Costo",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Existencia",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Ganancia",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "ValorInventario",
+                table: "Productos");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Costo",
+                table: "ProductosDetalle",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Ganancia",
+                table: "ProductosDetalle",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Costo",
+                table: "ProductosDetalle");
+
+            migrationBuilder.DropColumn(
+                name: "Ganancia",
+                table: "ProductosDetalle");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Costo",
+                table: "Productos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Existencia",
+                table: "Productos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Ganancia",
+                table: "Productos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Precio",
+                table: "Productos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ValorInventario",
+                table: "Productos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
