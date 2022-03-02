5cfaad735d74caa15ed85f734bca3e49af1bdf44
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-01 22:32:26 -0400

    Update Contexto.cs

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,20 +5,7 @@ namespace Productos_Detalle.DAL
 {
     public class Contexto : DbContext
     {
-
         public DbSet<Productos> Productos { get; set; } 
-
-        public Contexto()
-        {
-            
-        }
-        public Contexto(DbContextOptions<Contexto> options) : base(options)
-        {
-
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite(@"Data Source=Data/Productos.db");            
-        }
+        public Contexto(DbContextOptions<Contexto> options) : base(options){}
     }
 }
